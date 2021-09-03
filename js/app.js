b31f0245c28301f3dee0e930637ff22dcdaeacfc
Lorena Infante <m.lorenainfanter@gmail.com>
2021-09-03 18:13:01 -0400

    fetched names working

--- js/app.js
+++ js/app.js
@@ -13,9 +13,9 @@ function getPosts(){
         
         for(let i=0; i<h5.length; i++){
             post = posts[i].body; 
-            console.log(post);
+            
             h5[i].textContent = `"${post}"`;
-            console.log(h5);
+            
         }
   /*         //test for the first element
         let txt = document.querySelector("#testim-usr1"); 
@@ -25,25 +25,31 @@ function getPosts(){
         console.log("Error "+ `${error}`);
     })
 }
-
+//brings the names from the API
 function getNames(){
     const API_URL_usrs= 'https://jsonplaceholder.typicode.com/users';
-
     fetch (`${API_URL_usrs}`)
     .then(function(response){
         return response.json();
     })
     .then(function(usrs){
-        let usr = usrs[0].name;
-        const p = document.querySelector("#usr1");
-        p.innerHTML = `${usr}`;
+        let usr;
+        //brings all h5 titles inside testimonials-- as a NodeList
+        let p = document.querySelectorAll(".testimonials-->div>p");
+        
+        
+        for(let i=0; i<p.length; i++){
+            usr = usrs[i].name; 
+            
+            p[i].textContent = `${usr}`;
+            
+        }
     })
     .catch(function(error){
         console.log("Error "+ `${error}`);
     })
-
 }
 
 getPosts();
-getNames()
+getNames();
 
